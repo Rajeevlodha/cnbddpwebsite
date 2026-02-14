a4d46800c18ba32e9b305c3f8dd5538ccfed8e38
RAJEEV LODHA <lrajeevlodha@gmail.com>
2026-02-14 14:54:30 +0530

    added js file

---
+++ script.js
@@ -0,0 +1,9 @@
+const container = document.getElementById("productContainer");
+
+function scrollLeft() {
+    container.scrollLeft -= 300;
+}
+
+function scrollRight() {
+    container.scrollLeft += 300;
+}
